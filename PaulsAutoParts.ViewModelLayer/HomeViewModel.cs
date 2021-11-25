69cf7852ba2a838517857e8b0db7e83c274c10f4
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2021-11-25 12:54:37 +0000

    Revert "remove code for menu hiding from server side"
    This reverts commit 9408862b8857aadfb3f5ddee8790e8741769bd07.

--- PaulsAutoParts.ViewModelLayer/HomeViewModel.cs
+++ PaulsAutoParts.ViewModelLayer/HomeViewModel.cs
@@ -2,5 +2,15 @@ namespace PaulsAutoParts.ViewModelLayer
 {
   public class HomeViewModel : AppViewModelBase
   {
+    public enum Menus
+    {
+      None,
+      MyAccount,
+      Maintenance
+    }
+
+    #region Properties
+    public Menus MenuToDisplay { get; set; }
+    #endregion    
   }
 }

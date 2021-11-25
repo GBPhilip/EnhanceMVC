b27a1e8a1d3fe2194af04fc750a18879396f1bf4
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2021-11-25 12:10:20 +0000

    Add toggle for shopping search

---
+++ PaulsAutoParts/wwwroot/js/Shopping/ManageSearch.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+$(document).ready(function () {
+    $("#yearMakeModel")
+        .on("shown.bs.collapse", function () {
+            $("#nameCategory").collapse("hide");
+        });
+    $("#nameCategory")
+        .on("shown.bs.collapse", function () {
+            $("#yearMakeModel").collapse("hide");
+        });
+});

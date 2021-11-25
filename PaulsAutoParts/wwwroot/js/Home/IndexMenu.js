892ce79a2a6e19698c860b0102610746bf1e66b3
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2021-11-25 12:11:16 +0000

    Move  javascript to scripts area rather than head section

---
+++ PaulsAutoParts/wwwroot/js/Home/IndexMenu.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+let pageController = (function () {
+    function toggleMyAccountMenu() {
+        $("#myAccountMenu").toggleClass("d-none");
+        $("#maintenanceMenu").addClass("d-none");
+    }
+
+    function toggleMaintenanceMenu() {
+        $("#maintenanceMenu").toggleClass("d-none");
+        $("#myAccountMenu").addClass("d-none");
+    }
+
+    return {
+        "toggleMyAccountMenu": toggleMyAccountMenu,
+        "toggleMaintenanceMenu": toggleMaintenanceMenu
+    }
+})();

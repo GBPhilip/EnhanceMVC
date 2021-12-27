78b9aff0407152d5187b05867e78f5fd0a1512e5
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2021-12-27 20:28:14 +0000

    add year validation

--- PaulsAutoParts/wwwroot/js/VehicleType/pageController.js
+++ PaulsAutoParts/wwwroot/js/VehicleType/pageController.js
@@ -1,4 +1,24 @@
 ﻿let pageController = (function () {
+    function addValidationRules() {
+        let maxYear = new Date().getFullYear() + 2;
+        $.validator.addMethod("yearplusone",
+            function (value, element) {
+                return this.optional(element) || value < (maxYear);
+            });
+        $("form").validate({
+            rules: {
+                "SelectedEntity.Year": {
+                    yearplusone: true
+                }
+            },
+            messages: {
+                "SelectedEntity.Year": {
+                    yearplusone: "Year must be before " +
+                        maxYear
+                }
+            }
+        });
+    }
     function setSearchValues() {
         let searchValues = $("#SearchEntity_Make").val() +
             $("#SearchEntity_Model").val();
@@ -12,6 +32,7 @@
     return {
         "setSearchValues": setSearchValues,
         "setSearchArea": mainController.setSearchArea,
-        "isSearchFilledIn": mainController.isSearchFilledIn
+        "isSearchFilledIn": mainController.isSearchFilledIn,
+        "addValidationRules": addValidationRules
     }
 })();
